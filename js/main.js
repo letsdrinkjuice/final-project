4e2203946e8c422bd779509b7a968db8d65bcfe7
Jessica Straker <straker.jessica@gmail.com>
2016-05-25 22:32:34 +0100

    Submit with JS response
    finally managed to get js to respond to input submission. when email is
    submitted, user is prompted that it has been added to list. few style
    tweaks to submit button.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('it works');
+$('document').ready(function(){
+
+	//when i submit my email
+	//the form is hidden
+	//thank you appears
+	$('form').submit(function(event){
+		event.preventDefault();
+
+		required = ["#email"];
+
+		$('form').hide();
+		$('#glistThanks').fadeIn(750);
+
+	}); 
+});
