b71165dc6d438029292a2df8bd7911031f7e65e6
Jessica Straker <straker.jessica@gmail.com>
2016-05-28 10:56:56 +0100

    updated tweaks
    added event poster left table to index.html
    created premiere.html page
    renamed home.html to takeover to reflect content
    cleaned up sass code

--- js/main.js
+++ js/main.js
@@ -9,7 +9,41 @@ $('document').ready(function(){
 		required = ["#email"];
 
 		$('form').hide();
-		$('#glistThanks').fadeIn(750);
+		$('#glistThanks').fadeIn(750).on('click', function(event){
 
+			$('#rsvp').hide();
+		});
 	}); 
+
+	//when the mouse is moving, the overlay-skin is visible
+	// when the mouse stops, the overlay-skin is hidden
+	//siblings remain
+
+	$('.overlay-skin').bind('mousestop', function() {
+  	
+}); 
+
+
+
+//	$('.overlay-skin')[0].onmousemove = (function() {
+//	    var onmousestop = function() 
+//
+//	    if ( $('document').onmousemove(); ) {
+//	       $('.overlay-skin').show();
+//	    } else {
+//	       $('.overlay-skin').hide();
+//	        clearTimeout(thread);
+//	        thread = setTimeout(onmousestop);
+//	    };
+//	})();
+
+	//when user click premiere h2
+	//.prem-info div is displayed
+	//after 15secs .prem-info fade out
+
+	$('.more-info').on('click', function(event){
+		event.preventDefault();
+
+		$('.prem-info').fadeIn(750).delay(5000).fadeOut(750);
+	});
 });
